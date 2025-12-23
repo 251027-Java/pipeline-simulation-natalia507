fe24bc67c7b20c51f48b1c9ba257615988ba7563
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2025-12-23 17:13:38 -0600

    Completed Assignment

--- starter_code/PipelineSimulation.java
+++ starter_code/PipelineSimulation.java
@@ -2,6 +2,7 @@ package com.revature.lab;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class PipelineSimulation {
 
@@ -24,9 +25,14 @@ public class PipelineSimulation {
     }
 
     private static boolean runStage(String name, Supplier<Boolean> task) {
-        System.out.println("Running Stage: " + name + "...");
-        // TODO: Execute task and handle return value
-        return true;
+        boolean success = task.get();
+
+        if (!success) {
+            System.out.println("Stage " + name + " FAILED");
+        }
+
+        return success;
+
     }
 
     // TODO: Implement helper methods
@@ -34,4 +40,62 @@ public class PipelineSimulation {
     interface Supplier<T> {
         T get();
     }
+
+    private static boolean checkSourceCode() {
+        // Simulate checking source code
+
+        File sourceCode = new File("source_code.txt");
+        if (!sourceCode.exists()) {
+            System.out.println("Error");
+            return false;
+        }
+        else{
+            System.out.println("Compilation success");
+            return true;
+        }
+    }
+
+    private static boolean runRandomTests() {
+        // Simulate running tests
+        Random random = new Random();
+        int passedTests = random.nextInt(2);
+        if (passedTests == 0) {
+            System.out.println("Tests failed");
+            return false;
+        } else {
+            System.out.println("Tests passed");
+            return true;
+        }
+    }
+
+
+    private static boolean deployArtifact() {
+        try {
+            File artifact = new File("artifact.jar");
+            artifact.createNewFile();
+            System.out.println("Artifact packaged");
+        } catch (IOException e) {
+            System.out.println("Packaging failed");
+            return false;
+        }
+
+        File artifact = new File("artifact.jar");
+        File deployDir = new File("deploy");
+
+        if (!deployDir.exists()) {
+            deployDir.mkdir();
+        }
+
+        File deployedArtifact = new File(deployDir, "artifact.jar");
+
+        if (artifact.renameTo(deployedArtifact)) {
+            System.out.println("Artifact Deployed");
+            return true;
+        } else {
+            System.out.println("Deployment Failed");
+            return false;
+        }
+    }
+
+
 }
